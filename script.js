fc72d6942dec5cb9479e25b461325e2228f1ad99
birbwatcher <d@rkmind.cc>
2022-08-30 15:21:04 +0200

    feat: full layout with books implemented

--- script.js
+++ script.js
@@ -9,6 +9,12 @@ let headerWrapper = document.createElement("div");
 headerWrapper.classList.add('header-wrapper')
 let footerWrapper = document.createElement("div");
 footerWrapper.classList.add('footer-wrapper')
+let logo = document.createElement("div");
+logo.classList.add('logo');
+let cardContainer = document.createElement("div");
+cardContainer.classList.add('cards-container');
+let cart = document.createElement("div");
+cart.classList.add('cart');
 
 loadedPage.append(header);
 header.append(headerWrapper);
@@ -18,11 +24,31 @@ loadedPage.append(wrapper);
 loadedPage.append(footer);
 footer.append(footerWrapper);
 document.body.append(loadedPage);
+headerWrapper.append(logo)
+main.append(cardContainer);
+sidebar.append(cart)
 
-fetch('./books.json') //path to the file with json data
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            console.log(data);
-        });
+function createCard() {
+    for (i=0;i<books.length;i++) {
+        let card = document.createElement("div");
+        card.classList.add('card-item');
+        cardContainer.append(card);
+    }
+}
+
+let books;
+
+async function getBooks() {
+    let result = await fetch("./books.json");
+    let array = await result.json();
+    books = await array;
+}
+
+async function shopInit() {
+    await getBooks();
+    for (i=0;i<books.length;i++) {
+        createCard();
+    }
+}
+shopInit();
+
