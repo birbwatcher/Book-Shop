8db7db8a19af38cea757b8021977cb3082fe4bc4
birbwatcher <d@rkmind.cc>
2022-08-30 12:27:06 +0200

    fix: src excluded from gitignore

--- script.js
+++ script.js
@@ -2,16 +2,21 @@ let loadedPage = document.createDocumentFragment();
 let header = document.createElement("header")
 let main = document.createElement("main");
 let sidebar = document.createElement("aside");
+let footer = document.createElement("footer");
 let wrapper = document.createElement("div");
 wrapper.classList.add('wrapper')
 let headerWrapper = document.createElement("div");
 headerWrapper.classList.add('header-wrapper')
+let footerWrapper = document.createElement("div");
+footerWrapper.classList.add('footer-wrapper')
 
 loadedPage.append(header);
 header.append(headerWrapper);
 wrapper.append(main);
 main.append(sidebar);
 loadedPage.append(wrapper);
+loadedPage.append(footer);
+footer.append(footerWrapper);
 document.body.append(loadedPage);
 
 fetch('./books.json') //path to the file with json data
