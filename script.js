c76360141fb8e6b6eddbcef075cb206ea55d0fc9
birbwatcher <d@rkmind.cc>
2022-08-31 17:39:00 +0200

    feat: sidebar cart added

--- script.js
+++ script.js
@@ -1,22 +1,7 @@
 let loadedPage = document.createDocumentFragment();
-// let header = document.createElement("header");
-let main = document.createElement("main");
-let footer = document.createElement("footer");
 let wrapper = createElement("div","wrapper")
-let headerWrapper = createElement("div","header-wrapper")
-let footerWrapper = createElement("div","footer-wrapper")
-
-
 let cardContainer = createElement("div","cards-container")
 
-loadedPage.append(createHeader());
-wrapper.append(main);
-wrapper.append(createSidebar());
-loadedPage.append(wrapper);
-loadedPage.append(footer);
-footer.append(footerWrapper);
-document.body.append(loadedPage);
-main.append(cardContainer);
 
 function createElement(element, elementClass) {
     let newElement = document.createElement(element);
@@ -35,14 +20,47 @@ function createHeader() {
     logoImg.alt = "JS Book Store"
     logo.append(logoImg)
     headerWrapper.append(logo)
-    return header
+    loadedPage.append(header);
+}
+
+function createMain() {
+    let main = document.createElement("main");
+    wrapper.append(main);
+    main.append(cardContainer);
+    createSidebar();
 }
 
 function createSidebar() {
     let sidebar = document.createElement("aside");
     let cart = createElement("div","cart")
+    let yourCart = createElement("b", "your-cart");
+    let total = createElement("b", "total")
     sidebar.append(cart);
-    return sidebar;
+    yourCart.innerHTML = "Your cart:";
+    total.innerHTML = "Total:"
+    cart.append(yourCart);
+    cart.append(createCartContainer());
+    cart.append(total);
+    wrapper.append(sidebar)
+}
+
+function createCartContainer() {
+    let cartContainer = createElement("div","cart-container");
+    let dragImage = createElement("img","drag-image");
+    let title = document.createElement("b");
+    title.innerHTML = "Your cart is empty. Just drag and drop the book here";
+    dragImage.src = "./assets/dragndrop.png";
+    cartContainer.append(dragImage);
+    cartContainer.append(title);
+    return cartContainer;
+}
+
+function createFooter() {
+    let footer = document.createElement("footer");
+    let footerWrapper = createElement("div","footer-wrapper");
+
+    footer.append(footerWrapper);
+    return footer;
 }
 
 function createCard(x) {
@@ -73,12 +91,13 @@ function createCard(x) {
     }
 }
 
-function createCart() {
-    let cart = createElement("div","cart")
-    sidebar.append(cart);
-}
-
-let books;
+// function createCart() {
+//     let cart = createElement("div","cart")
+//     let yourCart = document.createElement("b");
+//     yourCart.innerHTML = "Your cart:";
+//     cart.append(yourCart);
+//     sidebar.append(cart);    
+// }
 
 async function getBooks() {
     let result = await fetch("./books.json");
@@ -87,8 +106,17 @@ async function getBooks() {
 }
 
 async function shopInit() {
+    createHeader();
+    createMain();
     await getBooks();
     createCard();
-    // createCart();
+    createFooter();
+
+    loadedPage.append(wrapper);
+    loadedPage.append(createFooter());
+    document.body.append(loadedPage);
 }
+
 shopInit();
+
+
