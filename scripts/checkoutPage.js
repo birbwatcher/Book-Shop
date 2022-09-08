ba46a418131b3699e8149ab1981dbd3f69b2752e
birbwatcher <d@rkmind.cc>
2022-09-08 14:43:46 +0200

    feat: forms on checkout page is created

--- scripts/checkoutPage.js
+++ scripts/checkoutPage.js
@@ -1,22 +1,78 @@
 import createElement from "./createElement.js";
 
 export default function createCheckout() {
-    let checkoutContainer = createElement("div","checkout-container")
+    let checkoutContainer = createElement("div","checkout-container");
+    let checkoutHeader = createElement("h1","checkout-header");
+    checkoutHeader.innerHTML = "Checkout";
     document.querySelector('main').append(checkoutContainer);
-    let cartName = createElement("h1","cart-name");
-    let name = createElement("input","input-name");
-    let surname = createElement("input","input-name");
-    let street = createElement("input","input-street");
-    let houseNum = createElement("input","input-house-number");
-    let apt = createElement("input","input-apt-number");
-
-    cartName.innerHTML = 'Cart';
-
-    checkoutContainer.append(cartName);
-    checkoutContainer.append(name);
-    checkoutContainer.append(surname);
-    checkoutContainer.append(street);
-    checkoutContainer.append(houseNum);
-    checkoutContainer.append(apt);
+    checkoutContainer.append(checkoutHeader);
+    let checkoutForm = createElement("form","checkout-form");
+    checkoutContainer.append(checkoutForm)
+    let userData = createElement("div","user-data");
+    let paymentType = createElement("div","payment-type");
+    checkoutForm.append(userData);
+    checkoutForm.append(paymentType);
     
+    let paymentHeader = createElement("h2","checkout-header");
+    paymentHeader.innerHTML = "Choose the payment type:"
+    paymentType.append(paymentHeader);
+
+    let giftType = createElement("div","gift-type");
+    let giftHeader = createElement("h2","gift-header");
+    giftHeader.innerHTML = "Choose 2 gifts: (optional)"
+    giftType.append(giftHeader);
+    checkoutForm.append(giftType);
+
+    userData.append(createInput("name","Name:", "text"));
+    userData.append(createInput("surname","Surname:", "text"));
+    userData.append(createInput("street","Your Street:", "text"));
+    userData.append(createInput("house-number","House Number:", "text"));
+    userData.append(createInput("flat-number","Flat Number:", "text"));
+    userData.append(createInput("delivery-date","Preferable Delivery Date:", "date"));
+    
+    // let cardPayment = createInput("by-card","By Card", "radio");
+    // let cashPayment = createInput("by-cash","By Cash", "radio")
+    // cardPayment.name = "payment";
+    // cashPayment.name = "payment";
+    paymentType.append(createPayment("card", "Card"));
+    paymentType.append(createPayment("cash", "Cash"));
+
+    giftType.append(chooseGift("gift", "pack as a gift"))
+    giftType.append(chooseGift("postcard", "add postcard"))
+    giftType.append(chooseGift("discount", "provide 2% discount to the next time"))
+    giftType.append(chooseGift("branded-pen", "branded pen or pencil"))
+}
+
+function createInput(name, labelName, inputtype) {
+    let block = createElement("div", name);
+    let label = document.createElement("label");
+    let input = document.createElement("input");
+    label.innerHTML = labelName;
+    input.type = inputtype;
+    block.append(label);
+    block.append(input);
+    return block;
+}
+
+function createPayment(name, labelName) {
+    let block = createElement("div", name);
+    let label = document.createElement("label");
+    let input = document.createElement("input");
+    label.innerHTML = labelName;
+    input.type = "radio";
+    input.name = "Payment";
+    block.append(input);
+    block.append(label);
+    return block;
+}
+
+function chooseGift(name, labelName) {
+    let block = createElement("div", name);
+    let label = document.createElement("label");
+    let input = document.createElement("input");
+    label.innerHTML = labelName;
+    input.type = "checkbox";
+    block.append(input);
+    block.append(label);
+    return block;
 }
